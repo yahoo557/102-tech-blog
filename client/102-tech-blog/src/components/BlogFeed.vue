34f23d05ffc0813422c89a42bc4dbc2366b5be80
baeklee <77774331+monjk557@users.noreply.github.com>
2022-12-17 21:29:43 +0900

    Feat. axios await 처리

--- client/102-tech-blog/src/components/BlogFeed.vue
+++ client/102-tech-blog/src/components/BlogFeed.vue
@@ -1,16 +1,27 @@
 <template>
-  <h1>{{ title }}</h1>
-  <p>{{ author }}</p>
-  <p>{{ date }}</p>
-  <h3>{{ summary }}</h3>
+  <div>
+    <ul class="wrap">
+      <li class="item">
+        <a  target="_blank">
+          <span class="tit">제목: {{ title }}</span>
+<!--          <span class="aut">작성자: {{user_id}}</span>-->
+<!--          <span class="aut">작성일: {{date}}</span>-->
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
 defineProps<{
-  title: string;
-  author: string;
-  summary: string;
-  date: Date;
-  id : number;
+
+  title: { type: string,  required: true};
+  // user_id: { type: string,  required: true};
+  // date: { type: string,  required: true};
+  // summary: { type: string,  required: true};
+  // img: { type: string,  required: false};
+
+  // src: { type: string,  required: false};
+
 }>();
 </script>
--- client/102-tech-blog/src/views/FeedView.vue
+++ client/102-tech-blog/src/views/FeedView.vue
@@ -1,28 +1,19 @@
+
 <template>
-  <div>
-    <Feed :title=""/>
-  </div>
+  <h1>Feed View</h1>
+  <Feed :title="data[0].title"/>
 </template>
 
-<script setup lang="ts">
-import Feed from "@/components/BlogFeed.vue";
 
-export default {
-  components: {
-    Feed
-  }
-}
+<script setup lang="ts">
+
+import Feed from"../components/BlogFeed.vue";
 import axios from "axios";
-axios({
-  url: "http://localhost:3000/app/board",
-  method: "GET",
-  withCredentials: true,
-})
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log("err : ", err);
-    });
+
+const getData = async ()=>{
+  let response = await axios.get("http://localhost:3000/app/board")
+  return await response.data.rows
+}
+const data = await getData()
 
 </script>
