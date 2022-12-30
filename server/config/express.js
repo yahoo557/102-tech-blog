6b986abc029cf4c860d20e2ddee4468dbbb0a057
baeklee <77774331+monjk557@users.noreply.github.com>
2022-12-30 23:31:59 +0900

    style: 띄어쓰기랑 줄간격 살짝 수정

--- server/config/express.js
+++ server/config/express.js
@@ -25,7 +25,7 @@ module.exports = function () {
     // TODO: 도메인을 추가할 경우 이곳에 Route를 추가하세요.
     require('../src/app/User/userRoute')(app);
     require('../src/app/board/boardRoute')(app);
-    // require('../src/app/Reply/replyRoute')(app);
+    require('../src/app/Reply/replyRoute')(app);
     // require('../src/app/Random/randomRoute')
     require('../src/app/Random/randomRoute')(app);
 
