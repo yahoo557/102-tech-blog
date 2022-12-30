aa231aedbce7e8963a0820acf5e9e6d8537421ef
baeklee <77774331+monjk557@users.noreply.github.com>
2022-12-30 23:32:49 +0900

    fix: 닉네임 reg, ip 일부분 표시 기능 오류 수정

--- server/src/app/Reply/replyRoute.js
+++ server/src/app/Reply/replyRoute.js
@@ -7,22 +7,18 @@
 
 module.exports = (app) =>{
     const reply = require('./replyControler')
-    // const jwtMiddleware = require('../../../config/jwtMiddleware');
-
-    // 0. 테스트
-    app.post('/app.reply/test', reply.replyTest);
 
     // 1. 댓글생성
-    app.post('/app/reply', reply.writeReply() );
+    app.post('/app/reply', reply.writeReply);
 
     // 2. 댓글 리스트 가져오기
-    app.get('/app/reply', reply.getReplyList());
+    app.get('/app/reply', reply.getReplyList);
 
     // 3. 댓글 수정
-    app.fetch('/app/reply/:id', reply.editReply());
+    app.patch('/app/reply/:id', reply.editReply);
 
     // 4. 댓글 삭제
-    app.delete('/app/reply/:id', reply.deleteReply());
+    app.delete('/app/reply/:id', reply.deleteReply);
 
 
 }
