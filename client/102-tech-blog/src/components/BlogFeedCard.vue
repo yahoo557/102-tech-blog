55d42a5e376f04d4d6d9c3b826188e8c41d6618a
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-09 22:10:09 +0900

    feat: props 생성 및 전달작업

--- client/102-tech-blog/src/components/BlogFeedCard.vue
+++ client/102-tech-blog/src/components/BlogFeedCard.vue
@@ -1,11 +1,25 @@
-<template>
-  <div>
+<script setup lang="ts">
+interface Post {
+  id?: number;
+  title?: string;
+  body?: string;
+  createdDate?: string;
+  user_id?: number;
+  viewCont?: number;
+}
+const postData = defineProps<Post>();
+console.log('in Card', postData.id, postData.title);
+</script>
 
+<template>
+  <div class="card-body">
+    <h4>{{ postData.title }}</h4>
+    <hr />
+    <p class="card-text">{{ postData.body }}</p>
   </div>
 </template>
 
-<script setup lang="ts">
 
+<style scoped>
 
-
-</script>
+</style>
