06eb713fb24dc1876570cb69d5f429dd9d3d269f
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-09 22:10:20 +0900

    feat: props 생성 및 전달작업

--- client/102-tech-blog/src/views/FeedView.vue
+++ client/102-tech-blog/src/views/FeedView.vue
@@ -1,11 +1,3 @@
-<template>
-  <div class="container pt-5">
-    <div>
-      <p class="fs-1 mb-0">{{ postData.title }}</p>
-    </div>
-  </div>
-</template>
-
 <script lang="ts">
 interface Data {
   data: string;
@@ -14,8 +6,10 @@ interface Data {
 
 import { useStorePost } from "@/compositions/useStorePost";
 import { httpGet } from "@/modules/http";
+import BlogFeedCard from "@/components/BlogFeedCard.vue";
 
 export default {
+  components: { BlogFeedCard },
   setup(): any {
     const { postData, setState } = useStorePost();
     const onSuccess = (data: Data) => {
@@ -25,9 +19,22 @@ export default {
       console.log('failed', data);
     };
     httpGet('/app/board', onSuccess, onFailed);
+    console.log(postData.value);
     return {
       postData,
     };
   },
 };
 </script>
+
+<template>
+  <div class="container pt-5">
+    <ul>
+      <li v-for="(post, index) in postData" v-bind:key="post">
+        <p>{{ index }}</p>
+        <h1>{{ post.title }}</h1>
+        <BlogFeedCard :postData="post" />
+      </li>
+    </ul>
+  </div>
+</template>
