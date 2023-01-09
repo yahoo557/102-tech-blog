482641be3bd03302241778290dd692b6feb39f85
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-09 14:57:19 +0900

    feat: http Post 모듈 생성

--- client/102-tech-blog/src/modules/http.ts
+++ client/102-tech-blog/src/modules/http.ts
@@ -1,23 +1,46 @@
-import axios from 'axios' ;
-import dotenv from 'dotenv';
+import axios from "axios";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default () => {
-  const BASE_URL = `${process.env.BASE_URL}:${process.env.BASE_PORT}/app`;
-  const httpGet = (api: string, onSuccess: Function, onFailed: Function) => {
-    const url = api.startsWith("http") ? api : BASE_URL + api;
-    axios.get(url).then((res) => {
-      if (res.status === 200) onSuccess(res.data);
-    });
+  const getUrl = (api: string): string => {
+    const BASE_URL = `${process.env.BASE_URL}:${process.env.BASE_PORT}/app`;
+    return api.startsWith("http") ? api : BASE_URL + api;
   };
 
-  // POST 메소드 모듈 제작중
-  const httpPost = (api: string, body: object, onSuccess: Function, onFailed: Function) => {
-    const url = api.startsWith("http") ? api : BASE_URL + api;
-    axios.post(url).then((res) => {});
+  const httpGet = (api: string, onSuccess: Function, onFailed: Function) => {
+    const url = getUrl(api);
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.status === 200) onSuccess(res.data);
+        if (res.status !== 200) onFailed(res.data);
+      })
+      .catch((err) => {
+        onFailed(err);
+      });
+  };
+
+  const httpPost = (
+    api: string,
+    body: object,
+    onSuccess: Function,
+    onFailed: Function
+  ) => {
+    const url = getUrl(api);
+    axios
+      .post(url, body)
+      .then((res) => {
+        if (res.status === 200) onSuccess(res.data);
+        if (res.status !== 200) onFailed(res.data);
+      })
+      .catch((err) => {
+        onFailed(err);
+      });
   };
   return {
     httpGet,
+    httpPost,
   };
 };
