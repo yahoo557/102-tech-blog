04db9c00f3f4b09eeb91f58f7ddceea87daec6d6
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-12 20:56:43 +0900

    feat: Post interface 설정

--- client/102-tech-blog/src/compositions/useStorePost.ts
+++ client/102-tech-blog/src/compositions/useStorePost.ts
@@ -1,24 +1,21 @@
 import { ref } from "vue";
-import type { Ref } from "vue";
+import type { readonly, Ref } from "vue";
 
 export const useStorePost = (): any => {
   interface Post {
-    title: string;
-    body: string;
-    createDate: String;
-    viewCount: number;
+    id?: number;
+    title?: string;
+    body?: string;
+    created_at?: string;
+    status?: string;
+    updated_at?: string;
+    user_id?: number;
+    view_count?: number;
+    like_count?: number;
   }
-  const state: Ref<Post> = ref({
-    title: "title",
-    body: "body",
-    createDate: "any",
-    viewCount: 0,
-  });
-  const setState = (post: Post) => {
-    state.value.title = post.title;
-    state.value.body = post.body;
-    state.value.createDate = post.createDate;
-    state.value.viewCount = post.viewCount;
+  const state: Ref<Post[]> = ref([]);
+  const setState = (rows: Post[]) => {
+    state.value = rows;
   };
   return {
     postData: state,
