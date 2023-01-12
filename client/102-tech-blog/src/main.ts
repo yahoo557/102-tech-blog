66f77c5b46b7f460b5d1fa025aa9f84f9460ddcc
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-12 20:56:25 +0900

    feat: 부트스트랩 설정

--- client/102-tech-blog/src/main.ts
+++ client/102-tech-blog/src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.esm.min.js";
+import "bootstrap";
 import "./assets/main.css";
 
 const app = createApp(App);
