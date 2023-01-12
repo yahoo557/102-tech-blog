fb8177916ba6e91fcdbb9ab23738bd4fef01e322
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-12 20:54:36 +0900

    docs: env 설정 삭제

--- client/102-tech-blog/.eslintrc.cjs
+++ client/102-tech-blog/.eslintrc.cjs
@@ -3,11 +3,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  "env": {
-    "node":true,
-    "browser": true,
-    "es2020": true
-},
+
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
