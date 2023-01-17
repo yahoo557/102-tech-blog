e7c2f5c2c9cd62cb9decd4dff84c285370c0f647
baeklee <77774331+monjk557@users.noreply.github.com>
2023-01-17 21:53:03 +0900

    fix: pool connection release 적용

--- server/src/app/Reply/replyProvider.js
+++ server/src/app/Reply/replyProvider.js
@@ -3,15 +3,21 @@ const replyDao = require("./replyDao")
 
 exports.retrieveReplyListPost = async (postId) =>{
     const connection = await pool.connect();
-    return await replyDao.selectReplyPost(connection, postId);
+    const result = await replyDao.selectReplyPost(connection, postId);
+    connection.release();
+    return result
 }
 
 exports.retrieveReplyListUser = async (userId) =>{
     const connection = await pool.connect();
-    return await replyDao.selectReplyUser(connection, userId);
+    const result = await replyDao.selectReplyUser(connection, userId);
+    connection.release();
+    return result
 }
 
 exports.retrieveReplyPassword = async (replyId, hashedPassword) => {
     const connection = await pool.connect()
-    return await replyDao.selectReplyPassword(connection, replyId, hashedPassword);
+    const result = await replyDao.selectReplyPassword(connection, replyId, hashedPassword);
+    connection.release();
+    return result
 }
