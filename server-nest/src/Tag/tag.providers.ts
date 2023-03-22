294421391e397cdad64ee45a31de3397772a2be8
baeklee <77774331+monjk557@users.noreply.github.com>
2023-03-22 23:13:58 +0900

    feat: Tag List 추가

--- server-nest/src/Tag/tag.providers.ts
+++ server-nest/src/Tag/tag.providers.ts
@@ -1,10 +1,15 @@
 import { DataSource } from 'typeorm';
-import {Tag} from './tag.entity'
+import {Tag,TagList} from './tag.entity'
 
 export const TagProviders=[
     {
         provide: "TAG_REPOSITORY",
         useFactory:(dataSource: DataSource) => dataSource.getRepository(Tag),
+        inject:["DATA_SOURCE"],
+    },
+    {
+        provide: "TAG_LIST_REPOSITORY",
+        useFactory:(dataSource: DataSource) => dataSource.getRepository(TagList),
         inject:["DATA_SOURCE"],
     }
 ]
