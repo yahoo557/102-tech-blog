121529760c7250d0a3ecdeb3b71a44d6dcedf540
baeklee <77774331+monjk557@users.noreply.github.com>
2023-03-22 23:11:10 +0900

    fix: Tag Container 정상 작동 수정

--- client/102-tech-blog/src/views/FeedView.vue
+++ client/102-tech-blog/src/views/FeedView.vue
@@ -5,13 +5,12 @@ interface Data {
 }
 
 import { useStorePost } from "@/compositions/useStorePost";
-
 import { httpGet } from "@/modules/http";
 import BlogFeedCard from "@/components/BlogFeedCard.vue";
-import TagView from "@/views/TagView.vue"
+import TagContainer from "@/components/TagContainer.vue"
 
 export default {
-  components: { BlogFeedCard },
+  components: { BlogFeedCard,TagContainer },
   setup(): any {
     const { postData, setState } = useStorePost();
     const onSuccess = (data: Data) => {
@@ -29,9 +28,10 @@ export default {
 </script>
 
 <template>
-  <TagView />
   <div class="container pt-5">
-  <div class="row row-cols-1 row-cols-sm-3 g-2 m-0">
+    <TagContainer />
+
+    <div class="row row-cols-1 row-cols-sm-3 g-2 m-0">
     <div class="col" v-for="(post, index) in postData" v-bind:key="index">
       <BlogFeedCard v-bind="post" />
     </div>
