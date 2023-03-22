ad9956c15b120ec76ca9e4da8cee9a98622c809c
baeklee <77774331+monjk557@users.noreply.github.com>
2023-03-22 23:14:05 +0900

    feat: Tag List 추가

--- server-nest/src/Tag/tag.service.ts
+++ server-nest/src/Tag/tag.service.ts
@@ -1,16 +1,28 @@
 import {Injectable, Inject, Logger} from '@nestjs/common';
 import { Repository } from 'typeorm';
-import {Tag} from './tag.entity'
+import { Tag, TagList } from "./tag.entity";
+
 
 export class TagService{
     constructor(
-        @Inject('TAG_REPOSITORY')
-        private tagRepository:Repository<Tag>
+      @Inject("TAG_LIST_REPOSITORY")
+      private tagListRepository:Repository<TagList>,
+      @Inject("TAG_REPOSITORY")
+      private tagRepository:Repository<Tag>,
+
     ) {}
 
     async getAllTag(): Promise<Tag[]> {
-        Logger.log("Request {/api/tag, GET}", "TagController")
         return await this.tagRepository.find();
     }
+    async getAllTagList(): Promise<TagList[]>{
+        return await this.tagListRepository.find();
+    }
 
+    async createTag(tag:Tag): Promise<boolean>{
+        await this.tagRepository.create(tag);
+        return true
+    }
 }
+
+
