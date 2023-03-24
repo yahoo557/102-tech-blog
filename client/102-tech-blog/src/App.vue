ca3d0772ff8ba640065fdbc80faa7c357092dd81
baeklee <77774331+monjk557@users.noreply.github.com>
2023-03-24 20:25:16 +0900

    feat: 네브바 홈버튼 포지션 수정

--- client/102-tech-blog/src/App.vue
+++ client/102-tech-blog/src/App.vue
@@ -3,12 +3,12 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav class = "position-relative">
-        <RouterLink class ="position-absolute top-50 start-50 translate-middle" to="/"><img src = "https://102-tech-blog.s3.ap-northeast-2.amazonaws.com/home-icon.png"/></RouterLink>
+  <header class = "">
+      <nav>
+        <RouterLink class = "d-flex justify-content-center mt-3" to="/">
+          <img style="height: 10vh" src = "https://102-tech-blog.s3.ap-northeast-2.amazonaws.com/hombutton.png" alt = "hot guy wearing sunglasses"/>
+        </RouterLink>
       </nav>
-    </div>
   </header>
   <RouterView />
 </template>
