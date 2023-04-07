3e8f5925ccbb948be1d386ba1b44821a08ed922d
baeklee <77774331+monjk557@users.noreply.github.com>
2023-04-07 23:30:33 +0900

    feat: add provide method to allow access from tag container component

--- client/102-tech-blog/src/views/FeedView.vue
+++ client/102-tech-blog/src/views/FeedView.vue
@@ -1,33 +1,27 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useStorePost } from "@/compositions/useStorePost";
-import { useStoreTagList} from "@/compositions/useStoreTagList";
+import { provide,Ref} from "vue";
+import { useStoreFeed } from "@/compositions/useStoreFeed";
 import { httpGet } from "@/modules/http";
 import BlogFeedCard from "@/components/BlogFeedCard.vue";
 import TagContainer from "@/components/TagContainer.vue"
+import type Data from "@/interface/dataInterface"
+import type Post from "@/interface/postInterface"
+const { feedData, setState } = useStoreFeed();
 
-interface Data {
-  data: string;
-  rows: object[];
-}
 
-const { postData, setState } = useStorePost();
-const onSuccess = (data: Data) => {
-  setState(data);
-};
-const onFailed = (data: Data) => {
-  console.log('failed', data);
-};
-httpGet('/api/board', onSuccess, onFailed);
+const onSuccess = (data: Data) => {setState(data);};
+const onFailed = (data: Data) => {console.log('failed', data);};
+httpGet(`/api/board/`, onSuccess, onFailed);
+provide("feedData", feedData as Ref<Post[]>);
 
 </script>
 
 <template>
-  <div class="container pt-5">
+  <div class="container pt-5 mb-3">
       <TagContainer />
-      <div class="row row-cols-1 row-cols-sm-3 g-2 m-0">
-      <div class="col" v-for="(post, index) in postData" v-bind:key="index">
-        <BlogFeedCard v-bind="post" />
+      <div class="row row-cols-sm-3 g-2 m-0" >
+      <div class="" v-for="(post, index) in feedData" v-bind:key="index">
+        <BlogFeedCard v-bind="post" v-if="post.visibility"/>
       </div>
   </div>
   </div>
