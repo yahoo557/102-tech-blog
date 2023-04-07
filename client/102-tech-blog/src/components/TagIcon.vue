7c1ad24ed2f8d85110b19918038863f13ad58747
baeklee <77774331+monjk557@users.noreply.github.com>
2023-04-07 23:26:18 +0900

    style: remove empty space

--- client/102-tech-blog/src/components/TagIcon.vue
+++ client/102-tech-blog/src/components/TagIcon.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 interface Props {
   id: number;
   created_at: string;
@@ -8,11 +7,11 @@ interface Props {
   imageUrl:string;
 }
 const tag = defineProps<Props>();
-
 </script>
+
 <template>
   <div class="form-check form-check-inline">
-    <input type="checkbox" class="btn-check" v-bind:id="'btn-check-' + tag.id" autocomplete="off" :value="tag.id" >
+    <input type="checkbox" class="btn-check" v-bind:id="'btn-check-' + tag.id" autocomplete="off" :value="tag.id" ref="tagRefs">
     <label class="btn btn-light rounded-pill border position-relative m-1 h-auto w-100"  v-bind:for="'btn-check-' + tag.id" >
       <img class="w-100" v-if="tag.imageUrl" :src="tag.imageUrl" :alt="tag.name" />
       <p v-else> {{ tag.name }}</p>
